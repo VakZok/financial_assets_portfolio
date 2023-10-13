c47075f3895340f3ebc109ef5d37d557fba5737e
VakZok <tobias.krebs1999@gmx.de>
2023-10-13 15:50:29 +0200

    Added basic setup

---
+++ src/main/java/hs/aalen/financial_assets_portfolio/FinancialAssetsPortfolioApplication.java
@@ -0,0 +1,13 @@
+package hs.aalen.financial_assets_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialAssetsPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialAssetsPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hs/aalen/financial_assets_portfolio/FinancialAssetsPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package hs.aalen.financial_assets_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAssetsPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
