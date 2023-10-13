818b4dda5cbeaf1c7230f384f890df15d4214ff4
VakZok <tobias.krebs1999@gmx.de>
2023-10-13 16:07:42 +0200

    Added Getter and Setter for domain class Message.java

--- src/main/java/hs/aalen/financial_assets_portfolio/domain/Message.java
+++ src/main/java/hs/aalen/financial_assets_portfolio/domain/Message.java
@@ -3,4 +3,20 @@ package hs.aalen.financial_assets_portfolio.domain;
 public class Message {
     private Long id;
     private String message;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
--- src/main/java/hs/aalen/financial_assets_portfolio/web/HelloController.java
+++ src/main/java/hs/aalen/financial_assets_portfolio/web/HelloController.java
@@ -1,5 +1,6 @@
 package hs.aalen.financial_assets_portfolio.web;
 
+import hs.aalen.financial_assets_portfolio.service.MessageService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1")
 public class HelloController {
+
+    private final MessageService messageService;
+
+    public HelloController(MessageService messageService) {
+        this.messageService = messageService;
+    }
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello(){
