1b5d628bf6f3716dba44b57b2d94774e7e363f94
VakZok <100447067+VakZok@users.noreply.github.com>
2023-10-24 16:33:06 +0200

    Fixed routing, added first draft of the impressum, and added mein_portfolio component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { RouterModule, Routes } from '@angular/router';
-
 import { NgModule } from '@angular/core';
-
-
 import { AddItemComponent} from "./modules/home/pages/add-item/add-item.component";
 import { AddShareComponent} from "./modules/home/pages/add-share/add-share.component";
+import { MeinPortfolioComponent} from "./modules/home/pages/mein-portfolio/mein-portfolio.component";
+import { ImpressumComponent} from "./modules/home/pages/impressum/impressum.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/asset_hinzuefuegen', pathMatch: 'full'}, // redirects to home page which is mein_portfolio
+  { path: '', redirectTo: '/mein_portfolio', pathMatch: 'full'}, // redirects to home page which is mein_portfolio
+  { path: 'mein_portfolio', component: MeinPortfolioComponent},
   { path: 'asset_hinzufuegen', component: AddItemComponent},
-  { path: 'add_share', component: AddShareComponent}
+  { path: 'add_share', component: AddShareComponent},
+  { path: 'impressum', component: ImpressumComponent}
 ];
 
 @NgModule({
