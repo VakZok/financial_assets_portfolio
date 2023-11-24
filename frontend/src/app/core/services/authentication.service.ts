c39931e7026f6d0aae07dba0f6f37aeb63da80c7
VakZok <100447067+VakZok@users.noreply.github.com>
2023-11-24 21:42:40 +0100

    Implemented authentication.service.ts

--- frontend/src/app/core/services/authentication.service.ts
+++ frontend/src/app/core/services/authentication.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {AccountModel} from "../models/account.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/v1/login';
+
+  constructor(private http: HttpClient) {
+  }
+
+  checkUsernameExists(userName: string): Observable<AccountModel> {
+    return this.http.head<AccountModel>(this.apiUrl + '/' + userName)
+  }
+
+  postCredentials(user: AccountModel): Observable<AccountModel> {
+    const headers = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.post<AccountModel>(this.apiUrl + '/post', user, {headers: headers})
+  }
 }
