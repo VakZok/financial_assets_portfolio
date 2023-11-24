f98d1f034f1830aa2708ad8af22c6fea1d217049
VakZok <100447067+VakZok@users.noreply.github.com>
2023-11-24 21:51:23 +0100

    Added basic login form

---
+++ frontend/src/app/modules/home/pages/login/login-form/login-form.component.ts
@@ -0,0 +1,52 @@
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AccountModel} from "../../../../../core/models/account.model";
+import {AuthenticationService} from "../../../../../core/services/authentication.service";
+import {Router} from "@angular/router";
+import {UsernameValidator} from "../../../../../core/validators/username-validator";
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent {
+  LoginForm: FormGroup;
+
+  errorMap = new Map<string, string>([
+    ['userName', ''],
+    ['password', '']
+  ]);
+
+  // Form Group Validator to ensure that password and username are not longer than 30 characters
+  constructor(private authenticationService: AuthenticationService,
+              private router: Router) {
+
+    /* Form Validation, check for completeness and sanity */
+    this.LoginForm = new FormGroup({
+      userName: new FormControl('', {
+        asyncValidators: [UsernameValidator(this.authenticationService)],
+        validators: [
+          Validators.required,
+          Validators.maxLength(30)
+        ],
+        updateOn: 'blur'
+      }),
+      password: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.maxLength(30)
+        ]
+      })
+    });
+  }
+
+  onSubmit() {
+    if (this.LoginForm.valid) {
+      console.log('Willkommen ' + String(userName) + '!', this.LoginForm.value);
+    } else {
+      // Form is invalid, display error messages or take appropriate action
+      console.log('Form is invalid. Please check the fields.');
+    }
+  }
+}
