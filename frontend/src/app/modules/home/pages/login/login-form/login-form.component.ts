99785107427c0d8b987471da4940e14e90bef5ec
VakZok <100447067+VakZok@users.noreply.github.com>
2023-12-01 13:21:44 +0100

    Added login.model.ts and finished login-form.component.ts

--- frontend/src/app/modules/home/pages/login/login-form/login-form.component.ts
+++ frontend/src/app/modules/home/pages/login/login-form/login-form.component.ts
@@ -1,10 +1,22 @@
 import {Component, ViewChild} from '@angular/core';
 import {AbstractControl, FormBuilder, FormControl, FormGroup, FormGroupDirective, Validators} from '@angular/forms';
-import {AccountModel} from "../../../../../core/models/account.model";
 import {AuthenticationService} from "../../../../../core/services/authentication.service";
 import {Router} from "@angular/router";
 import {UsernameValidator} from "../../../../../core/validators/username-validator";
 import {first} from "rxjs";
+import {LoginModel} from "../../../../../core/models/login.model";
+
+async function waitForFormNotPending(formGroup: FormGroup): Promise<void> {
+  return new Promise<void>((resolve) => {
+    formGroup.updateValueAndValidity()
+    const subscription = formGroup.statusChanges.subscribe((status) => {
+      if (status !== 'PENDING') {
+        subscription.unsubscribe(); // Unsubscribe when not pending
+        resolve();
+      }
+    });
+  });
+}
 
 @Component({
   selector: 'app-login-form',
@@ -31,25 +43,12 @@ export class LoginFormComponent {
       username: new FormControl('', {
         asyncValidators: [UsernameValidator(this.authenticationService)],
         validators: [
-          Validators.required,
-          Validators.maxLength(30)
-        ],
-        updateOn: 'blur'
+          Validators.required
+        ]
       }),
       password: new FormControl('', [
-        Validators.required,
-        Validators.maxLength(30)]
-      )
-    })
-  }
-
-  // send get request after blurring username input field
-  async onBlurUsername() {
-    this.LoginForm.statusChanges.pipe(
-      first(status => status !== 'PENDING')).subscribe(status => {
-      if (this.LoginForm.controls['username'].errors?.['UsernameExists']){
-        this.errorMap.set('username', 'Dieser Benutzername ist bereits vergeben');
-      }
+        Validators.required
+      ])
     })
   }
 
@@ -62,12 +61,66 @@ export class LoginFormComponent {
     }
   }
 
-  onSubmit() {
-    if (this.LoginForm.valid) {
-      console.log('Willkommen!', this.LoginForm.value);
+  async onSubmit() {
+    // loop over input form and remove leading/trailing whitespaces
+    for (const controlName in this.LoginForm.controls) {
+      if (this.LoginForm.controls.hasOwnProperty(controlName)) {
+        const control = this.LoginForm.get(controlName);
+        if (control?.value != null) {
+          control?.setValue(control?.value.trim())
+        }
+      }
+    }
+
+    // map errors that are not async
+    if (this.LoginForm.controls['username'].errors?.['required']) {
+      this.errorMap.set('username', 'Bitte geben Sie Ihren Benutzernamen ein');
     } else {
-      // Form is invalid, display error messages or take appropriate action
-      console.log('Form is invalid. Please check the fields.');
+      this.errorMap.set('username', '');
+    }
+
+    if (this.LoginForm.controls['password'].errors?.['required']) {
+      this.errorMap.set('password', 'Bitte geben Sie Ihr Passwort ein');
+    } else {
+      this.errorMap.set('password', '');
+    }
+
+    // wait until Form Validation has finished
+    await waitForFormNotPending(this.LoginForm)
+
+    // map async username error
+    if (this.LoginForm.invalid) {
+      if (this.LoginForm.controls['username'].errors?.['pItemExists'] == false) {
+        this.errorMap.set('username', 'Benutzer mit diesem Benutzernamen existiert nicht.');
+      }
+    }
+
+    if (this.LoginForm.valid) {
+      // initialize errors if form is valid
+      for (let [key, error] of this.errorMap) {
+        this.errorMap.set(key, '');
+      }
+
+      // create loginDTO from LoginForm
+      const loginDTO: LoginModel = {
+        username: this.LoginForm.controls['username'].value || '',
+        password: this.LoginForm.controls['password'].value || '',
+      }
+
+      // on success reset form
+      this.authenticationService.postCredentials(loginDTO).subscribe({
+        next: (data) => {
+          this.LoginForm.reset();
+          this.form.resetForm();
+        },
+
+        // if backend-validation produces exceptions on postCredentials, set them on the errorMap
+        error: (errors) => errors.error.forEach((item: any) => {
+          this.LoginForm.get(item.name)?.setErrors(item.message)
+          this.errorMap.set(item.name, item.message);
+        }),
+        complete: () => this.router.navigate(['meinPortfolio'])
+      })
     }
   }
 }
